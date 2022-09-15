5401da67c50b7fdd214fee0a9d82e730cc194248
mathbueeno <mathbueeno@gmail.com>
2022-09-15 14:29:15 -0300

    first commit

---
+++ Configuration.cs
@@ -0,0 +1,9 @@
+namespace Blog
+{
+    public static class Configuration
+    {
+        // Token - JWT - Json Web Token
+        public static string JwtKey { get; set; } = "356951662";
+    }
+}
+
